7d4fea5bcc8fe9faa62daad76b9b435c6a2e3720
alpa <alfaariizii2712@gmail.com>
2026-05-30 20:54:26 +0700

    Add files via upload

---
+++ 1_data_cleaning.sql
@@ -0,0 +1,41 @@
+-- Cek Overview Awal
+SELECT
+	*
+FROM student_analysist sa
+LIMIT 10;
+
+-- Cleaning Data
+UPDATE student_analysist sa
+SET 
+    AttendanceRate = IF(AttendanceRate = '', NULL, AttendanceRate),
+    ParentalSupport = IF(ParentalSupport = '', NULL, ParentalSupport),
+    FinalGrade = IF(FinalGrade = '', NULL, FinalGrade),
+    `Online Classes Taken` = IF(`Online Classes Taken` = '', NULL, `Online Classes Taken`);
+
+SELECT 
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN StudentID IS NULL THEN 1 ELSE 0 END) AS missing_id,
+    SUM(CASE WHEN Name IS NULL THEN 1 ELSE 0 END) AS missing_name,
+    SUM(CASE WHEN AttendanceRate IS NULL THEN 1 ELSE 0 END) AS missing_attendance_rate,
+    SUM(CASE WHEN ParentalSupport IS NULL THEN 1 ELSE 0 END) AS missing_parental,
+    SUM(CASE WHEN FinalGrade IS NULL THEN 1 ELSE 0 END) AS missing_final_grade
+FROM student_analysist sa;
+
+DELETE FROM student_analysist
+WHERE StudentID IS NULL OR StudentID = '';
+
+UPDATE student_analysist sa 
+SET 
+    `Study Hours` = IF(`Study Hours` < 0, NULL, `Study Hours`),
+    `Attendance (%)` = IF(`Attendance (%)` > 100, NULL, `Attendance (%)`);
+
+UPDATE student_analysist sa 
+SET 
+    ParentalSupport = COALESCE(NULLIF(ParentalSupport, ''), 'Unknown'),
+    Gender = COALESCE(NULLIF(Gender, ''), 'Unknown');
+
+DELETE FROM student_analysist  
+WHERE FinalGrade IS NULL OR FinalGrade = '';
+
+DELETE FROM student_analysist 
+WHERE Gender = 'Unknown';
---
+++ 2_exploratory_analysis.sql
@@ -0,0 +1,80 @@
+-- Cek overview setelah cleaning
+SELECT
+	*
+FROM student_analysist sa 
+LIMIT 10;
+
+-- Nilai Akhir Siswa Berdasarkan Gender
+-- Apakah terdapat kesenjangan performa akademik yang signifikan antara siswa laki-laki dan perempuan?
+
+SELECT 
+    Gender,
+    COUNT(*) AS total_siswa,
+    ROUND(AVG(FinalGrade), 2) AS rata_rata_nilai_akhir,
+    ROUND(MIN(FinalGrade), 2) AS nilai_terendah,
+    ROUND(MAX(FinalGrade), 2) AS nilai_tertinggi
+FROM student_analysist sa
+GROUP BY Gender;
+
+-- Pengharuh Waktu Belajar Terhadap Nilai Akhir Siswa
+-- Bagaimana dampak durasi belajar mandiri mingguan terhadap hasil akhir siswa?
+
+SELECT 
+    CASE 
+        WHEN StudyHoursPerWeek < 10 THEN '1. Rendah (<10 jam/minggu)'
+        WHEN StudyHoursPerWeek BETWEEN 10 AND 20 THEN '2. Sedang (10-20 jam/minggu)'
+        ELSE '3. Tinggi (>20 jam/minggu)'
+    END AS Kategori_Jam_Belajar,
+    COUNT(*) AS Total_Siswa,
+    ROUND(AVG(FinalGrade), 2) AS Rata_Rata_Nilai
+FROM student_analysist sa
+GROUP BY 1
+ORDER BY Kategori_Jam_Belajar;
+
+-- Pengaruh Tingkat Kehadiran Terhadap Nilai Akhir Siswa
+-- Bagaimana dampak tingkat kehadiran di kelas terhadap hasil akhir siswa?
+
+SELECT 
+    CASE 
+        WHEN AttendanceRate < 75 THEN '1. Kurang (<75%)'
+        WHEN AttendanceRate BETWEEN 75 AND 89 THEN '2. Cukup (75-89%)'
+        ELSE '3. Baik (>=90%)'
+    END AS Kategori_Kehadiran,
+    COUNT(*) AS Total_Siswa,
+    ROUND(AVG(FinalGrade), 2) AS Rata_Rata_Nilai
+FROM student_analysist sa 
+WHERE AttendanceRate IS NOT NULL
+GROUP BY 1
+ORDER BY Kategori_Kehadiran;
+
+-- Pengaruh Dukungan Orang Tua Terhadap Nilai Akhir Siswa
+-- Seberapa besar peran dukungan orang tua dalam mendongkrak pencapaian akademik anak?
+
+SELECT 
+    ParentalSupport,
+    COUNT(*) AS Total_Siswa,
+    ROUND(AVG(FinalGrade), 2) AS Rata_Rata_Nilai
+FROM student_analysist sa 
+WHERE ParentalSupport IS NOT NULL AND ParentalSupport != 'Unknown'
+GROUP BY ParentalSupport
+ORDER BY Rata_Rata_Nilai DESC;
+
+-- Segmentasi Siswa Di Luar Jam Sekolah
+-- "Apakah siswa yang terlalu banyak mengambil kegiatan (Ekskul + Kelas Online) justru kelelahan (burnout) sehingga nilainya turun? Atau justru mereka yang paling berprestasi karena pandai mengatur waktu?"
+
+SELECT 
+    CASE 
+        WHEN `Online Classes Taken` = 'True' AND ExtracurricularActivities > 0 THEN '1. Well-Rounded (Ekskul & Kelas Online)'
+        WHEN `Online Classes Taken` = 'True' AND ExtracurricularActivities = 0 THEN '2. Digital Learner (Hanya Kelas Online)'
+        WHEN `Online Classes Taken` = 'False' AND ExtracurricularActivities > 0 THEN '3. Traditional Active (Hanya Ekskul)'
+        ELSE '4. Minimalist (Tidak Keduanya)'
+    END AS Profil_Keterlibatan_Siswa,
+    COUNT(*) AS Total_Siswa,
+    ROUND(AVG(FinalGrade), 2) AS Rata_Rata_Nilai_Akhir,
+    ROUND(AVG(StudyHoursPerWeek), 2) AS Rata_Rata_Jam_Belajar_Per_Minggu,
+    ROUND(AVG(AttendanceRate), 2) AS Rata_Rata_Kehadiran_Sekolah
+FROM student_analysist sa 
+WHERE `Online Classes Taken` IS NOT NULL 
+  AND `Online Classes Taken` != ''
+GROUP BY 1
+ORDER BY Rata_Rata_Nilai_Akhir DESC;
